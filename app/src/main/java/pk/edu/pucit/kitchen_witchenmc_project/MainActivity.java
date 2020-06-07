f9e0608f496162316ba278e50c134fda936a0340
Mubarra Naz <47411183+MubarraNaz@users.noreply.github.com>
2020-06-07 21:15:15 +0500

    welcome screen created

--- app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/MainActivity.java
+++ app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/MainActivity.java
@@ -3,12 +3,30 @@ package pk.edu.pucit.kitchen_witchenmc_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+Button signUpBtn, signInBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signUpBtn=findViewById(R.id.btnSignUp);
+        signInBtn =findViewById(R.id.btnSignIn);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
