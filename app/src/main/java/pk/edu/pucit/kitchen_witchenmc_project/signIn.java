352a1aa5682960cad1a3195200f18343a2cb695a
Mubarra Naz <47411183+MubarraNaz@users.noreply.github.com>
2020-07-21 06:28:08 +0500

    DBHelper deleted

--- app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/signIn.java
+++ app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/signIn.java
@@ -3,21 +3,16 @@ package pk.edu.pucit.kitchen_witchenmc_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import pk.edu.pucit.kitchen_witchenmc_project.model.DBHelper;
 import pk.edu.pucit.kitchen_witchenmc_project.model.User;
-import android.database.sqlite.SQLiteDatabase;
 
 public class signIn extends AppCompatActivity {
 
-    DBHelper db;
     EditText mailTxt,passTxt;
     Button signIn;
     String mail, pass;
@@ -39,16 +34,10 @@ public class signIn extends AppCompatActivity {
                     mDialog.setMessage("Signing In, Please Wait!");
                     mDialog.show();
                     User user= new User(mail, pass);
-                    Boolean email=db.signIn(mail,pass);
-                    if(email==true){
+                    //if user exist
                         Toast.makeText(signIn.this,"Signed In Successfully!",Toast.LENGTH_SHORT).show();
-                    }
-
                     //else for user existence
-                    else
-                    {
-                        Toast.makeText(signIn.this,"Email Already Exists!",Toast.LENGTH_SHORT).show();
-                    }
+                        Toast.makeText(signIn.this,"Email Does not Exists!",Toast.LENGTH_SHORT).show();
                      mDialog.dismiss();
                     //Toast.makeText(signIn.this,"Signed In Successfully!",Toast.LENGTH_SHORT).show();
 
