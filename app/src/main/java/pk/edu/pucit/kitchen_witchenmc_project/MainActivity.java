0f7ee764ab25aa2dc327392ae46f272d014f4482
Mubarra Naz <47411183+MubarraNaz@users.noreply.github.com>
2020-07-21 17:14:18 +0500

    signIn process updated
    now main activity contains sign in process

--- app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/MainActivity.java
+++ app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/MainActivity.java
@@ -2,33 +2,91 @@ package pk.edu.pucit.kitchen_witchenmc_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import pk.edu.pucit.kitchen_witchenmc_project.model.User;
 
 public class MainActivity extends AppCompatActivity {
 
-Button signUpBtn, signInBtn;
+    EditText mailTxt,passTxt;
+    Button signIn;
+    TextView signUpLnk;
+    String email, passw;
+    DBHelper sqldb;
+    ProgressDialog sDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signUpBtn=findViewById(R.id.btnSignUp);
-        signInBtn =findViewById(R.id.btnSignIn);
+        mailTxt=findViewById(R.id.email);
+        passTxt=findViewById(R.id.psw);
+        signIn=findViewById(R.id.strt);
+        signUpLnk=findViewById(R.id.btnSignUp);
 
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent signIn=new Intent(MainActivity.this,signIn.class);
-                startActivity(signIn);
-            }
-        });
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
+        signUpLnk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent signUp=new Intent(MainActivity.this,signUp.class);
                 startActivity(signUp);
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email=mailTxt.getText().toString();
+                passw=passTxt.getText().toString();
+                if(email.isEmpty() || passw.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Empty fields remaining", Toast.LENGTH_SHORT).show();
+                }
+                else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toast.makeText(MainActivity.this, "Please enter a valid Email Address", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    sDialog=new ProgressDialog(MainActivity.this);
+                    sDialog.setMessage("Signing In, Please Wait!");
+                    sDialog.show();
+                    User user= new User(email, passw);
+                    boolean login=signInUsr(user);
+                    sDialog.dismiss();
+                    clearForm();
+                    if (login) {
+                        user.setName(sqldb.getUserName(user.getMail()));
+                        //go to home
+                    }
+                }
+
+
+            }
+
+            private boolean signInUsr(User user) {
+                try{
+                    sqldb=new DBHelper(MainActivity.this);
+                    if (sqldb.signIn(user)){
+                        Toast.makeText(MainActivity.this,"Signed In Successfully!",Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this,"Email or password incorrect",Toast.LENGTH_SHORT).show();
+
+                    }
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this,"Error Occurred Signing in, retry!",Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            private void clearForm() {
+                mailTxt.getText().clear();
+                passTxt.getText().clear();
             }
         });
     }
