606cf58e12e540cbcfd86b7965c55c1f7faf7b09
Mubarra Naz <47411183+MubarraNaz@users.noreply.github.com>
2020-07-21 06:19:51 +0500

    signUp complete

--- app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/signUp.java
+++ app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/signUp.java
@@ -3,11 +3,15 @@ package pk.edu.pucit.kitchen_witchenmc_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 import pk.edu.pucit.kitchen_witchenmc_project.model.User;
 
@@ -17,6 +21,8 @@ public class signUp extends AppCompatActivity {
     String usr,mail, pass;
     Button signUp;
     DBHelper db;
+    //SQLiteDatabase mdb=openOrCreateDatabase("KitchenWitchen",null, null, 1);
+    ProgressDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,31 +31,47 @@ public class signUp extends AppCompatActivity {
         txtMail=findViewById(R.id.mail);
         txtPass=findViewById(R.id.pass);
         signUp=findViewById(R.id.start);
-        usr=txtName.getText().toString();
-        pass=txtPass.getText().toString();
-        mail=txtMail.getText().toString();
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ProgressDialog mDialog=new ProgressDialog(signUp.this);
-                mDialog.setMessage("Signing Up, Please Wait!");
-                mDialog.show();
-                User user= new User(usr, mail, pass);
-        //verify and add data to database
-                Boolean email=db.signIn(mail,pass);
-                if(email==true){
-                    Boolean insert=db.insert(mail,pass);
-                    Toast.makeText(signUp.this,"Signed In Successfully!",Toast.LENGTH_SHORT).show();
+                usr=txtName.getText().toString();
+                pass=txtPass.getText().toString();
+                mail=txtMail.getText().toString();
+                if(mail.isEmpty() || pass.isEmpty() || usr.isEmpty()){
+                    Toast.makeText(signUp.this, "Empty fields remaining", Toast.LENGTH_SHORT).show();
                 }
-                else
-                {
-                    Toast.makeText(signUp.this,"Email Already Exists!",Toast.LENGTH_SHORT).show();
+                else if(!Patterns.EMAIL_ADDRESS.matcher(mail).matches()){
+                    Toast.makeText(signUp.this, "Please enter a valid Email Address", Toast.LENGTH_SHORT).show();
                 }
+                else{
+                    mDialog=new ProgressDialog(signUp.this);
+                    mDialog.setMessage("Signing Up, Please Wait!");
+                    mDialog.show();
+                    User user= new User(usr, mail, pass);
+                    //verify and add data to database
+                    db=new DBHelper(signUp.this);
+                    signUpUsr(user);
+                    clearForm();
+                }
+            }
 
+            private void clearForm() {
+                txtName.getText().clear();
+                txtMail.getText().clear();
+                txtPass.getText().clear();
+            }
+
+            private void signUpUsr(User usr){
+                if (db.signUp(usr)){
+                    Toast.makeText(signUp.this,"Signed Up Successfully!",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(signUp.this,"Email Already Exists!",Toast.LENGTH_SHORT).show();
+
+                }
                 mDialog.dismiss();
-                Toast.makeText(signUp.this,"Signed Up Successfully!",Toast.LENGTH_SHORT).show();
-                //else for user existence
+
             }
         });
     }
