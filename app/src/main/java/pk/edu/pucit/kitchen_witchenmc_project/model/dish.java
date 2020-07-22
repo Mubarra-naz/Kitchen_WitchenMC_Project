f1ee5900c7943299dd9f6c39ca639239088b0078
Mubarra Naz <47411183+MubarraNaz@users.noreply.github.com>
2020-07-22 23:44:40 +0500

    Create dish.java

---
+++ app/src/main/java/pk/edu/pucit/kitchen_witchenmc_project/model/dish.java
@@ -0,0 +1,52 @@
+package pk.edu.pucit.kitchen_witchenmc_project.model;
+
+public class dish {
+    String name;
+    String img;
+    String ingredients;
+    int price;
+
+    public dish(String name, String img, String ingredients, int price) {
+        this.name = name;
+        this.img = img;
+        this.ingredients = ingredients;
+        this.price = price;
+    }
+    public dish(String name, String img, int price){
+        this.name = name;
+        this.img = img;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
